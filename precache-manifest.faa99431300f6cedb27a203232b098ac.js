a0b2c994474de478408415c3212329b952eb853e
haralwit <haralwit@gmail.com>
2020-10-21 17:16:53 +0200

    Updates

---
+++ precache-manifest.faa99431300f6cedb27a203232b098ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6432e14071ad4731dff3afe5276aa931",
+    "url": "/norkart_workshop/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/norkart_workshop/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "e9e5a55f19a527636873",
+    "url": "/norkart_workshop/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/norkart_workshop/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/norkart_workshop/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e5a55f19a527636873",
+    "url": "/norkart_workshop/static/js/main.d2c26a61.chunk.js"
+  },
+  {
+    "revision": "b623e4760ccd0215dac7",
+    "url": "/norkart_workshop/static/js/runtime-main.0381604e.js"
+  }
+]);
